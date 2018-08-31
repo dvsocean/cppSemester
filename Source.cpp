dad181b5eeb8304fbdacf516db78fd7305437022
CISP 31 <cisp31@BCT.msac.mtsac.edu>
2018-08-30 20:40:46 -0700

    k

---
+++ Source.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+/*
+mulitline comment
+*/
+//starting point for programs
+void main() {
+	//cout << "Please enter a number \n";
+	//int userInput;
+	//string myString = "Oceans string";
+	//bool myBool = false;
+
+	//cin >> userInput;
+
+	//cout << "You entered " << userInput << " ";
+
+	int a, b;
+	a = 10;
+	b = 11;
+	float res = (float)a / (float)b;
+
+	cout << res << endl;
+}
