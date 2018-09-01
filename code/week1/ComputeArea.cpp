9ac52a5110842a7b13aaadaa8b6784379e09fbfd
Ocean <dvsocean@icloud.com>
2018-09-01 16:20:30 -0700

    adding simple try

---
+++ code/week1/ComputeArea.cpp
@@ -0,0 +1,16 @@
+#include <iostream> 
+using namespace std; 
+
+int main(){
+double radius;
+double area;
+
+radius = 20;
+
+area = radius * radius * 3.14159;
+
+cout << "The area is " << area << endl;
+
+return 0; 
+
+}
