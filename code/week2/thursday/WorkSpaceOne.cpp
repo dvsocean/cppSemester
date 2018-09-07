100f92dc776c3c7e2fc33d6c4a653df415a2fb3c
Ocean <dvsocean@icloud.com>
2018-09-07 07:00:46 -0700

    added thursday night workspace

---
+++ code/week2/thursday/WorkSpaceOne.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    //binary operators
+    //unary operators
+    //increment operators 
+    //decrement opertators 
+
+    // int x= 5;
+    // int y = ++x + 5;
+    // cout << "x is " << x << " y is " << y << endl;
+
+    // double salesTax = 4.99;
+
+    // cout << salesTax << "\n";
+
+    cout << "Enter number " << endl;
+    int x;
+    cin >> x;
+    string s = " IT WORKS IT WORKS";
+
+    if(x % 2 == 0){
+        cout << "Even" << endl;
+        cout << s << endl;
+    }
+        if(x % 2 != 0){
+        cout << "Odd" << endl;
+        }
+
+
+
+
+    return 0;
+}
